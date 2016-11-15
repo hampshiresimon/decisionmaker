060c51692184d25cfcc9f99d0376120d5ac0f733
Simon Hampshire <hampshire.simon@btinternet.com>
2016-11-15 21:35:50 +0000

    Initial client set-up with web pack

---
+++ Client/src/entry.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+ReactDOM.render(
+  <b />simon</b>,
+  document.getElementById('app')
+);
