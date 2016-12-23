7455f502674fe9ce7031d66a573d2eecd648f5a8
Simon Hampshire <hampshire.simon@btinternet.com>
2016-12-23 12:53:59 +0000

    Use localhost for listening

--- Server/config.development.js
+++ Server/config.development.js
@@ -1,6 +1,6 @@
 var config = {
     port: 3000,
-    url: 'www.decision-maker.co.uk',
+    url: '127.0.0.1',
     mongo: 'mongod',
     mongoUrl: 'mongodb://localhost/decisionmakerdev',
     secretKey: '4d693f79475f663a786c4a6f26',
